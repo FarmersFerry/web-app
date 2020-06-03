90a3708479ceccb5e34da25ad808908185494854
Matt Hunt <mattlethargic@gmail.com>
2020-06-03 12:17:10 +0100

    Updates

---
+++ precache-manifest.b28c3cb7ec76036714f73afa2a4ea9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac79edaedf2e32f41ddbc11405717aa1",
+    "url": "/web-app/index.html"
+  },
+  {
+    "revision": "99be7e293364b6943b35",
+    "url": "/web-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e14ad421d7011872a310",
+    "url": "/web-app/static/js/2.4bb81967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-app/static/js/2.4bb81967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99be7e293364b6943b35",
+    "url": "/web-app/static/js/main.9da28525.chunk.js"
+  },
+  {
+    "revision": "b2b0e22eec4cebc65499",
+    "url": "/web-app/static/js/runtime-main.495e4c17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
