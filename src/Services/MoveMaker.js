e5fdffec3505ac79e72d6537d39fa1854ad0f606
Matt Hunt <mattlethargic@gmail.com>
2020-06-03 18:57:24 +0100

    Adding move makes

---
+++ src/Services/MoveMaker.js
@@ -0,0 +1,63 @@
+const buildMoves = (geese, corn) => {
+
+    if (geese === 1 && corn == 2) {
+        return getCaseOne('goose', 'corn');
+    }
+
+    if (geese === 2 && corn == 1) {
+        return getCaseOne('corn', 'goose');
+    }
+
+    if (geese > 2 && corn === 0) {
+        return getCaseTwo(geese, 'geese');
+    }
+
+    if (geese === 0 && corn > 2) {
+        return getCaseTwo(corn, 'corn');
+    }
+
+    if (geese === 1 && corn === 1) {
+        return getCaseThree();
+    }
+
+    return [];
+}
+
+const getCaseOne = (small, large) => {
+    const moves = [];
+
+    moves.push({ take: small, direction: 'away' });
+    moves.push({ take: null, direction: 'home' });
+    moves.push({ take: large, direction: 'away' });
+    moves.push({ take: small, direction: 'home' });
+    moves.push({ take: large, direction: 'away' });
+    moves.push({ take: null, direction: 'home' });
+    moves.push({ take: small, direction: 'away' });
+    return moves;
+}
+
+const getCaseTwo = (numberOfThings, nameOfThings) => {
+    const moves = [];
+
+    for (let i = 0; i < numberOfThings; i++) {
+        moves.push({ take: nameOfThings, direction: 'away' });
+        moves.push({ take: null, direction: 'home' });
+    }
+
+    moves.pop();
+
+    return moves;
+}
+
+const getCaseThree = () => {
+    const moves = [];
+
+    moves.push({ take: 'goose', direction: 'away' });
+    moves.push({ take: null, direction: 'home' });
+    moves.push({ take: 'corn', direction: 'away' });
+
+    return moves;
+}
+
+
+export default { buildMoves };
