495957997a49923e512a7e7fa32ee5eacff11e20
Robert Marshall <robert.marshall@madetech.com>
2020-06-04 11:47:29 +0100

    Updates

--- precache-manifest.212c62cb34acb722f59a0913ba60169c.js
+++ precache-manifest.212c62cb34acb722f59a0913ba60169c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "da8f4cc85d70f40405edb5afd7cef840",
+    "revision": "839e752426fd10c86a31634deaf03132",
     "url": "/web-app/index.html"
   },
   {
-    "revision": "ccfe3b63fecf6c2cfd6c",
-    "url": "/web-app/static/css/main.617d6fb9.chunk.css"
+    "revision": "8b77a94404443a358760",
+    "url": "/web-app/static/css/main.08df789c.chunk.css"
   },
   {
     "revision": "262183e9624b946e7018",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/web-app/static/js/2.1c138a2c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ccfe3b63fecf6c2cfd6c",
+    "revision": "8b77a94404443a358760",
     "url": "/web-app/static/js/main.cb61308d.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-app/precache-manifest.69f44885b7ee35f3ec4779d6a93507fc.js"
+  "/web-app/precache-manifest.212c62cb34acb722f59a0913ba60169c.js"
 );
 
 self.addEventListener('message', (event) => {
