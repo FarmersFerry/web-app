6bf16bc1018f00bd4afa5f4d55f0f03d647c304e
Robert Marshall <robert.marshall@madetech.com>
2020-06-04 10:22:36 +0100

    Trip styling

--- src/Components/Trip/Trip.js
+++ src/Components/Trip/Trip.js
@@ -1,55 +1,72 @@
 import React from "react";
+import "./Trip.scss";
 import moveMaker from "../../Services/GooseCornTripBuilder";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowLeft, faCrow, faMale, faLeaf } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faArrowRight,
+	faArrowLeft,
+	faCrow,
+	faMale,
+	faLeaf,
+} from "@fortawesome/free-solid-svg-icons";
 
 class Moves extends React.Component {
 	state = { moves: [] };
 
-	constructor(props) { 
+	constructor(props) {
 		super(props);
 
 		this.state = { moves: moveMaker.buildMoves(props.geese, props.corn) };
 	}
 
 	componentWillReceiveProps(nextProps) {
-		this.setState({ moves: moveMaker.buildMoves(nextProps.geese, nextProps.corn) });
+		this.setState({
+			moves: moveMaker.buildMoves(nextProps.geese, nextProps.corn),
+		});
 	}
 
-	convertItemToIcon(item) { 
-		switch (item) { 
+	convertItemToIcon(item) {
+		switch (item) {
 			case "corn":
-				return <FontAwesomeIcon icon={faLeaf} />;
+				return <FontAwesomeIcon className="corn" icon={faLeaf} />;
 			case "goose":
 				return <FontAwesomeIcon icon={faCrow} />;
 		}
-		return <FontAwesomeIcon icon={faMale} />
+		return <FontAwesomeIcon icon={faMale} />;
 	}
 
 	render() {
-		const items = []
+		const items = [];
 
-		this.state.moves.forEach(element => {
+		this.state.moves.forEach((element) => {
 			let arrow = <FontAwesomeIcon icon={faArrowRight} />;
-			if (element.direction === 'home') {
-				arrow =  <FontAwesomeIcon icon={faArrowLeft} />;
+			if (element.direction === "home") {
+				arrow = <FontAwesomeIcon icon={faArrowLeft} />;
 			}
 
 			let take = element.take;
-			if(!take) {
-				take='nothing';
+			if (!take) {
+				take = "nothing";
 			}
 
 			take = this.convertItemToIcon(take);
 
-			items.push(<tr><td>{take}</td><td>{arrow}</td></tr>)
+			items.push(
+				<tr>
+					<td>{take}</td>
+					<td>{arrow}</td>
+				</tr>
+			);
 		});
 
-		return (<div><h2>Trips</h2><table>
-			<tbody>
-				{items}
-			</tbody>
-		</table></div>);
+		return (
+			<div>
+				<h2>Trips</h2>
+				<table class="trips">
+					<tbody>{items}</tbody>
+				</table>
+			</div>
+		);
 	}
 }
 
